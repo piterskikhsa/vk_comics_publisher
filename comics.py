26440ae4e06de7300620326f1754b9e598bcb8e2
Питерских Сергей Алексеевич <spiterskih@kplatform.ru>
2019-05-22 09:41:41 +0300

    create download random comics function

--- comics.py
+++ comics.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import requests
 from dotenv import load_dotenv
@@ -20,10 +21,23 @@ def download_image(image_link, image_name):
     response.raise_for_status()
     with open(image_name, 'wb') as out_file:
         out_file.write(response.content)
+    return image_name
+
+
+def get_json_from_url(url, params={}):
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.json()
 
 
 def get_random_comics_id():
-    pass
+    url = 'http://xkcd.com/info.0.json'
+    comics_json = get_json_from_url(url)
+    try:
+        max_comics_id = comics_json['num']
+    except KeyError:
+        return
+    return random.choice(range(max_comics_id))
 
 
 def get_comment(comics_data):
@@ -31,13 +45,25 @@ def get_comment(comics_data):
 
 
 def get_comics(comics_id):
-    pass
+    url = 'http://xkcd.com/{id}/info.0.json'.format(id=comics_id)
+    return get_json_from_url(url)
 
 
 def get_upload_url(VK_TOKEN):
     method = 'photos.getWallUploadServer'
-    response = ''
+    url = 'https://api.vk.com/method/{}'.format(method)
+    params = {'access_token': VK_TOKEN, 'v': 5.95}
+    response = get_json_from_url(url, params=params)
+    print(response)
     return response['upload_url']
+
+
+def download_random_comics(img_folder):
+    random_id = get_random_comics_id()
+    comics_json  = get_comics(random_id)
+    output_name = '{}/{}'.format(img_folder, comics_json['title'])
+    image_name = download_image(comics_json['img'], output_name)
+    return  (image_name, get_comment(comics_json))
 
 
 def upload_img():
@@ -61,6 +87,8 @@ def main():
     vk_url = f'https://api.vk.com/method/{METHOD_NAME}'
     playload = {'access_token': os.getenv('VK_TOKEN'), 'v': 5.95}
     response = requests.get(vk_url, params=playload)
+    print(download_random_comics('image'))
+    print(get_upload_url(os.getenv('VK_TOKEN')))
 
 
 if __name__ == '__main__':
